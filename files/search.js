ac5e33cac3dd90a2b2bd8da7ee912d38ebd79474
kpenkov98 <kpenk209@mtroyal.ca>
2023-12-03 12:48:52 -0700

    added radio buttons and blur to search field

--- files/search.js
+++ files/search.js
@@ -48,23 +48,55 @@ function removeDuplicate(data) {
   return uniqueArray;
 }
 
-function searchSong(searchBox) {
-  let songDetails;
-
-  if (searchBox == null) {
-    songDetails = "Song Not Found";
-  } else {
-    songDetails = song.find(searchBox);
-  }
-}
-
 //react to a song title being clicked
 function songTitleClick(e) {
   const row = e.target.parentNode;
   const id = row.dataset.song;
   const selectedSong = musicTable.find(
-    musicTable => musicTable.song_id == id
+    (musicTable) => musicTable.song_id == id
   );
 
   console.log(selectedSong.title);
 }
+
+//function to blur other options when radio button is selected
+function blurOptions(clicked) {
+  const title = document.getElementById("titleSearch");
+  const artist = document.getElementById("artistSearch");
+  const year = document.getElementById("yearSearch");
+  const genre = document.getElementById("genreSearch");
+
+  if (clicked === "titleSearch") {
+    artist.style.filter = "blur(2px)";
+    year.style.filter = "blur(2px)";
+    genre.style.filter = "blur(2px)";
+    title.style.filter = '';
+  } else if (clicked === "artistSearch") {
+    title.style.filter = "blur(2px)";
+    year.style.filter = "blur(2px)";
+    genre.style.filter = "blur(2px)";
+    artist.style.filter = '';
+  } else if (clicked === "yearSearch") {
+    artist.style.filter = "blur(2px)";
+    title.style.filter = "blur(2px)";
+    genre.style.filter = "blur(2px)";
+    year.style.filter = '';
+  } else if (clicked === "genreSearch") {
+    artist.style.filter = "blur(2px)";
+    year.style.filter = "blur(2px)";
+    title.style.filter = "blur(2px)";
+    genre.style.filter = '';
+  }
+}
+
+//function that searches for titles
+function searchTitle() {}
+
+//function that searches for artists
+function searchArtist() {}
+
+//function that searches for genres
+function searchGenre() {}
+
+//function that selects years
+function searchYear() {}
